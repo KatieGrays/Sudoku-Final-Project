45e8a2454e181586852834d88af0b9cd1e1cc84d
nickastonge <nstonge@ufl.edu>
2025-04-19 12:53:46 -0400

    board select and click functions

--- board.py
+++ board.py
@@ -9,33 +9,35 @@ class Board:
         self.height = height
         self.screen = screen
         self.difficulty = difficulty
-    #     Constructor for the Board class.
-    #     screen is a window from PyGame.
-    #     difficulty is a variable to indicate if the user chose easy medium, or hard.
-    #
+        self.box_w = width / 9
+        self.box_h = height / 9
+        self.selected_cell = None
+
     def draw(self):
         for i in range(1, 3):
-            pygame.draw.line(self.screen, "black",(0, (self.height // 3) * i),
-                             (self.width, (self.height // 3) * i), 3)
-            pygame.draw.line(self.screen, "black",((self.width // 3) * i, 0),
-                             ((self.width // 3) * i, self.height), 3)
+            pygame.draw.line(self.screen, "black",(0, (self.height / 3) * i),
+                             (self.width, (self.height / 3) * i), 3)
+            pygame.draw.line(self.screen, "black",((self.width / 3) * i, 0),
+                             ((self.width / 3) * i, self.height), 3)
         for i in range(1, 9):
             if i == 3 or i == 6:
                 continue
-            pygame.draw.line(self.screen, "black", (0, (self.height // 9) * i),
-                         (self.width, (self.height // 9) * i), 1)
-            pygame.draw.line(self.screen, "black", ((self.width // 9) * i, 0),
-                             ((self.width // 9) * i, self.height), 1)
+            pygame.draw.line(self.screen, "black", (0, self.box_h * i),
+                         (self.width, self.box_h * i), 1)
+            pygame.draw.line(self.screen, "black", (self.box_w * i, 0),
+                             (self.box_w * i, self.height), 1)
 
-    #     Draws an outline of the Sudoku grid, with bold lines to delineate the 3x3 boxes.
-    #     Draws every cell on this board.
-    #
-    #
-    # def select(self, row, col)
+    def select(self, row, col):
+        self.selected_cell = row, col
     #     Marks the cell at (row, col) in the board as the current selected cell.
     #     Once a cell has been selected, the user can edit its value or sketched value.
     #
-    # def click(self, row, col)
+    def click(self, x, y):
+        col = int(x // self.box_w)
+        row = int(y // self.box_h)
+        if row <= 8 and col <= 8:
+            return row, col
+        return None
     #     If a tuple of (x,y) coordinates is within the displayed board,
     # this function returns a tuple of the (row, col) of the cell which was clicked.
     # Otherwise, this function returns None.
@@ -82,7 +84,7 @@ window_height = 600
 screen = pygame.display.set_mode((window_width, window_height))
 
 # Initialize Board
-board = Board(600, 600, screen, "easy")
+board = Board(window_width, window_height, screen, "easy")
 
 # Set the window title
 pygame.display.set_caption("Pygame Window")
@@ -94,6 +96,11 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            x, y = event.pos
+            # print(board.click(x, y))
+            row, col = board.click(x, y)
+            board.select(row, col)
 
     # Fill the screen with a color (e.g., white)
     screen.fill((255, 255, 255))
